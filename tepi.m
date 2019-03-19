e6c1d18f61ba2df045fe946ea3d5e562e709dc49
unknown <you@example.com>
2019-03-19 01:45:57 -0700

    Penambahan codig tepi

---
+++ tepi.m
@@ -0,0 +1,6 @@
+I = imread('E:/PENGOLAHAN CITRA DIGITAL/CITRA/rice.png');
+canny  = edge(I,'canny');
+sobel  = edge(I,'sobel');
+robert = edge(I,'roberts');
+prewit = edge(I,'prewitt');
+imshow(canny);
